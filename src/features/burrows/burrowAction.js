dbf8910f050b8f03e19729b5972ecbed4d194fcd
IshaniNakarani12 <ishaninakarani12@gmail.com>
2024-06-04 13:28:40 +1000

    added search and sign in

--- src/features/burrows/burrowAction.js
+++ src/features/burrows/burrowAction.js
@@ -1,5 +1,7 @@
-import { postNewBurrow } from "./burrowAxios";
+import { getAllBooksAction, getSingleBookAction } from "../books/bookAction";
+import { fetchBurrows, postNewBurrow } from "./burrowAxios";
 import { toast } from "react-toastify";
+import { setBurrows } from "./burrowSlice";
 
 export const addNewBurrowAction = (obj) => async (dispatch) => {
   const pending = postNewBurrow(obj);
@@ -14,5 +16,15 @@ export const addNewBurrowAction = (obj) => async (dispatch) => {
 
   if (status) {
     //fetch the seleted book
+    dispatch(getAllBooksAction());
+  }
+};
+
+export const fetchBurrowsAction = () => async (dispatch) => {
+  const { status, burrows } = await fetchBurrows();
+  console.log(status, burrows);
+
+  if (status === "success") {
+    dispatch(setBurrows(burrows));
   }
 };
--- src/features/burrows/burrowAxios.js
+++ src/features/burrows/burrowAxios.js
@@ -12,21 +12,21 @@ export const postNewBurrow = async (obj) => {
   return apiProcesser(axiosObj);
 };
 
-// export const fetchBurrows = async (isPrivate) => {
-//   const axiosObj = {
-//     method: "get",
-//     url: isPrivate ? burrowEP + "/all" : burrowEP,
-//     isPrivate: true,
-//   };
-//   return apiProcesser(axiosObj);
-// };
-// export const fetchSingleBurrow = async (_id) => {
-//   const axiosObj = {
-//     method: "get",
-//     url: burrowEP + "/" + _id,
-//   };
-//   return apiProcesser(axiosObj);
-// };
+export const fetchBurrows = async () => {
+  const axiosObj = {
+    method: "get",
+    url: burrowEP,
+    isPrivate: true,
+  };
+  return apiProcesser(axiosObj);
+};
+export const fetchSingleBurrow = async (_id) => {
+  const axiosObj = {
+    method: "get",
+    url: burrowEP + "/" + _id,
+  };
+  return apiProcesser(axiosObj);
+};
 
 // export const updateABurrow = async (obj) => {
 //   const axiosObj = {
